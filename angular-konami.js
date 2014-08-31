058de9ae5a77b8a3d65a449a6f488ed928ece9ab
Lucas Constantino <lucas@taller.net.br>
2014-08-31 01:27:46 -0300

    Created angular-konami

---
+++ angular-konami.js
@@ -0,0 +1,63 @@
+/**
+ * Konami Code directive for AngularJS
+ * @version v0.0.1
+ * @license MIT License, http://www.opensource.org/licenses/MIT
+ */
+
+angular.module('konami', [])
+  .directive('konami', ['$document', function($document) {
+    var konamiKeys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
+    return {
+      restrict: 'A',
+      link: function(scope, element, attr) {
+
+        if (!attr.konami) {
+          throw('Konami directive must receive an expression as value.');
+        }
+
+        // Let user define a custom code.
+        if (attr.konamiCode) {
+          var konamiKeys = scope.$eval(attr.konamiCode);
+        }
+
+        var keyIndex = 0;
+
+        /**
+         * Fired when konami code is type.
+         */
+        function activated() {
+          if ('konamiOnce' in attr) stopListening();
+          // Execute expression.
+          scope.$eval(attr.konami);
+        }
+
+        /**
+         * Handle keydown events.
+         */
+        function keydown(e) {
+          if (e.keyCode === konamiKeys[keyIndex++]) {
+            if (keyIndex === konamiKeys.length) {
+              keyIndex = 0;
+              activated();
+            }
+          } else {
+            keyIndex = 0;
+          }
+        };
+
+        /**
+         * Stop to listen typing.
+         */
+        function stopListening() {
+          $document.off('keydown', keydown);
+        }
+
+        // Start listening to key typing.
+        $document.on('keydown', keydown);
+
+        // Stop listening when scope is destroyed.
+        scope.$on('$destroy', stopListening);
+      }
+    };
+  }]);
